a2e05f4753029cde574ec83aa3177477a2d855b6
michelemeiira <102440706+michelemeiira@users.noreply.github.com>
2022-04-13 00:16:46 -0300

    Add files via upload

---
+++ Criando um Banco Digital com Java e Orientação a Objetos/src/br/esig/com/nujava/dominio/Pessoa.java
@@ -0,0 +1,57 @@
+package br.esig.com.nujava.dominio;
+
+import java.util.Date;
+
+/** Esta entendidade representa uma Pessoa.
+ * 
+ * Uma pessoa possui CPF único. <p><br />
+ * Uma pessoa também faz um relacionamento com {@link Genero}. <p><br />
+ * 
+ * @authora Michele Meira
+ * @since 16/05/1992
+ * @version 1.0
+ */
+public class Pessoa {
+	private String nome;
+	
+	private Date dataNascimento;
+	
+	private String cpf;
+	
+	private Genero genero;
+	
+	public Pessoa() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+}
